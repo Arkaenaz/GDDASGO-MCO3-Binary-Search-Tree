232ed1a9230533f52a0453fe9d59544082e173ae
ALTelesto <83495459+ALTelesto@users.noreply.github.com>
2023-04-02 00:15:07 +0800

    progress

--- bst.h
+++ bst.h
@@ -17,28 +17,89 @@ typedef struct {
 	sNode *pRoot;
 } bst;
 
-bst* create() {
-	// Your code here
+sNode* createNode(int x) {
+    sNode* newNode = (sNode*)malloc(sizeof(sNode));
+	
+    newNode->data = x;
+    newNode->pLeft = NULL;
+    newNode->pRight = NULL;
+
+    return newNode;
 }
 
-void insert(bst *t, int x) {
-	// Your code here
+bst* create() {
+	bst* pTree = (bst*)malloc(sizeof(bst));
+    pTree->pRoot = NULL;
+    return pTree;
+}
+
+void insert(bst* t, int x) {
+	if (t->pRoot == NULL) {
+        t->pRoot = createNode(x);
+        return;
+    }
+    sNode* pCurrent = t->pRoot;
+    while (pCurrent != NULL) {
+        if (x < pCurrent->data) {
+            if (pCurrent->pLeft == NULL) {
+                pCurrent->pLeft = createNode(x);
+                return;
+            }
+            pCurrent = pCurrent->pLeft;
+        } 
+		else if (x > pCurrent->data) {
+            if (pCurrent->pRight == NULL) {
+                pCurrent->pRight = createNode(x);
+                return;
+            }
+            pCurrent = pCurrent->pRight;
+        } 
+		else {
+            return;
+        }
+    }
 }
 
 sNode* search(bst *t, int x) {
-	// Your code here
+	sNode *pCurrent = t->pRoot;
+
+    while (pCurrent != NULL) {
+        if (x == pCurrent->data){
+			return pCurrent;
+		}
+        else if (x < pCurrent->data){
+			pCurrent = pCurrent->pLeft;
+		}
+        else{
+			pCurrent = pCurrent->pRight;
+		}
+    }
+
+    return NULL;
 }
 
 void inorder(sNode *pPointer) {
-	// Your code here
+	if (pPointer != NULL) {
+        inorder(pPointer->pLeft);
+        printf("%d ", pPointer->data);
+        inorder(pPointer->pRight);
+    }
 }
 
 void preorder(sNode *pPointer) {
-	// Your code here
+	if (pPointer != NULL) {
+        printf("%d ", pPointer->data);
+        preorder(pPointer->pLeft);
+        preorder(pPointer->pRight);
+    }
 }
 
 void postorder(sNode *pPointer) {
-	// Your code here
+	if (pPointer != NULL) {
+        postorder(pPointer->pLeft);
+        postorder(pPointer->pRight);
+        printf("%d ", pPointer->data);
+    }
 }
 
 sNode* maximum(sNode *pPointer) {
